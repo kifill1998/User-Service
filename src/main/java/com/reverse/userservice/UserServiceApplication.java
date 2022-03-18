32ba52e497b9731a54054f5e7fedcadc4ff23751
kifill1998 <97985598+kifill1998@users.noreply.github.com>
2022-03-18 17:14:52 -0400

    Update UserServiceApplication.java

--- src/main/java/com/reverse/userservice/UserServiceApplication.java
+++ src/main/java/com/reverse/userservice/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceApplication {
-
+//Test Commit
     @Generated
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
