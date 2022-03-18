88439a0fb37b360447cf394e36117688e15bc88b
kifill1998 <97985598+kifill1998@users.noreply.github.com>
2022-03-18 17:09:07 -0400

    Update SecurityConfigure.java

--- src/main/java/com/reverse/userservice/SecurityConfigure.java
+++ src/main/java/com/reverse/userservice/SecurityConfigure.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
+    //Test Commit
 
     @Override
     @Generated
